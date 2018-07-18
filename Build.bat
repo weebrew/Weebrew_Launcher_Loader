306327f60ae0e6b2298df45693aaa6161c37b64f
weebrew <41343600+weebrew@users.noreply.github.com>
2018-07-18 09:56:45 -0700

    Fixed build.bat

--- Build.bat
+++ Build.bat
@@ -1,1 +1,1 @@
-makerom -f cia -o Weebrew-Launcher.cia -rsf cia.rsf -target t -exefslogo -elf Weebrew-Launcher.elf -icon Weebrew-Launcher.smdh -banner banner.bin
+makerom -f cia -o Weebrew-Launcher.cia -rsf cia.rsf -target t -exefslogo -elf hblauncher_loader-1.3.elf -icon hblauncher_loader-1.3.smdh -banner banner.bin
